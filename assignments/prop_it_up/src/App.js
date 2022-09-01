3c2a6ac3afc53cba90b433d7a558fd60259ae0d1
m-smith15 <mpsmith1215@gmail.com>
2022-09-01 02:17:23 -0400

    prop_it_up commit

---
+++ assignments/prop_it_up/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+
+import PersonCard from './components/PersonCard.js'
+
+function App() {
+  return (
+    <div className="App">
+      <PersonCard firstName={"Jane"} lastName={"Doe"} age={420} hairColor={"Blue"}/>
+      <PersonCard firstName={"John"} lastName={"Smith"} age={333} hairColor={"Brick"}/>
+      <PersonCard firstName={"Millard"} lastName={"Fillmore"} age={444} hairColor={"Malard-Brown"}/>
+      <PersonCard firstName={"Maria"} lastName={"Smith"} age={555} hairColor={"Green"}/>
+    </div>
+  );
+}
+
+export default App;
