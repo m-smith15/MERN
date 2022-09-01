d1a9b58f57b1ba974f143a502fb92d45a0359ab6
m-smith15 <mpsmith1215@gmail.com>
2022-09-01 01:15:36 -0400

    first_jsx commit - (check node mods in ignore)

---
+++ assignments/first_jsx/src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+
+function App() {
+  return (
+    <>
+    <h1>Hello Dojo!</h1>
+    <h2>Things I need to do:</h2>
+    <ul>
+      <li>Learn React</li>
+      <li>Climb Mountain</li>
+      <li>Run marathon</li>
+      <li>Feed ze dog</li>
+    </ul>
+    </>
+  );
+}
+
+export default App;
