3ab50553553ce779f532771b873f8d51ddca5094
m-smith15 <mpsmith1215@gmail.com>
2022-09-02 14:40:32 -0400

    big_inversion practice commit

---
+++ assignments/big_inversion/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './App.css';
+
+import PersonCard from './Components/PersonCard';
+
+
+function App() {
+  return (
+    <div className="App">
+      <PersonCard firstName={"Jane"} lastName={"Doe"} age={420} hairColor={"Blue"}/>
+      <PersonCard firstName={"John"} lastName={"Smith"} age={333} hairColor={"Brick"}/>
+      <PersonCard firstName={"Millard"} lastName={"Fillmore"} age={444} hairColor={"Malard-Brown"}/>
+      <PersonCard firstName={"Maria"} lastName={"Smith"} age={555} hairColor={"Green"}/>
+      </div>
+  );
+}
+
+export default App;
