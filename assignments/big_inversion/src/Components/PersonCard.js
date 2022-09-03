8c2c4dd688926cb7a7aa63c1cb2abe25c9cffa16
m-smith15 <mpsmith1215@gmail.com>
2022-09-03 00:44:39 -0400

    adding birthday count function

--- assignments/big_inversion/src/Components/PersonCard.js
+++ assignments/big_inversion/src/Components/PersonCard.js
@@ -1,10 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
+
 const PersonCard = props => {
+    const [age, setAge] = useState(props.age);
+
+    const incrimentAge = () => {
+        setAge(age + 1);
+    }
+
     return(
         <div>
             <h1>{ props.lastName }, { props.firstName }</h1>
-            <p>Age: { props.age }</p>
+            <p>Age: { age }</p>
             <p>Hair Color: { props.hairColor }</p>
+            <button onClick={ incrimentAge }>Birthday Button</button>
         </div>
     );
 }
