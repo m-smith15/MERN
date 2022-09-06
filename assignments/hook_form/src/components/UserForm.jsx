b0eee635673db7b32554b7cc5f747b792bf20f79
m-smith15 <mpsmith1215@gmail.com>
2022-09-06 17:50:49 -0400

    updating for more_form assignment!

--- assignments/hook_form/src/components/UserForm.jsx
+++ assignments/hook_form/src/components/UserForm.jsx
@@ -8,10 +8,40 @@ const UserForm = (props) => {
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
     const [confpassword, setConfPassword] = useState("");
+    //adding validations
+    const [firstnameError, setfirstnameError] = useState("");
+    const [lastnameError, setlastnameError] = useState("");
+    const [emailError, setemailError] = useState("");
+    const [passwordError, setpasswordError] = useState("");
+    const [confpasswordError, setConfPasswordError] = useState("");
 
     const createUser = (e) => {
         //preventDefault stops default behavior, important for usestate
         e.preventDefault();
+
+        //validation checks! - since this is updated in real time I don't want to handle it each time it changes 
+        //console.log(firstname);
+        if(firstname.length < 2){
+            setfirstnameError("first name must be greater than 2 characters")
+        } else {
+            setfirstnameError("")
+        }
+        if(lastname.length < 2){
+            setlastnameError("last name must be greater than 2 characters")
+        } else {
+            setlastnameError("")
+        }
+        if(email.length < 2){
+            setemailError("first name must be greater than 2 characters")
+        } else {
+            setemailError("")
+        }
+        if(password.length < 8){
+            setpasswordError("password must be greater than 8 characters")
+        }
+        if (password != confpassword){
+            setConfPasswordError("passwords must match!")
+        }
         const newUser = { firstname, lastname, email, password, confpassword };
         console.log("Welcome", newUser);
     };
@@ -23,22 +53,37 @@ const UserForm = (props) => {
             <div>
                 <label>first name: </label>
                 <input type="text" onChange={(e) => setFirstname(e.target.value)} />
+                {
+                    firstnameError ? <p style={{color: 'red'}}>{firstnameError}</p> : ' '
+                }
             </div>
             <div>
                 <label>last name: </label>
                 <input type="text" onChange={(e) => setLastname(e.target.value)} />
+                {
+                    lastnameError ? <p style={{color: 'red'}}>{lastnameError}</p> : ' '
+                }
             </div>
             <div>
                 <label>Email Address: </label>
                 <input type="text" onChange={(e) => setEmail(e.target.value)} />
+                {
+                    emailError ? <p style={{color: 'red'}}>{emailError}</p> : ' '
+                }
             </div>
             <div>
                 <label>Password: </label>
                 <input type="text" onChange={(e) => setPassword(e.target.value)} />
+                {
+                    passwordError ? <p style={{color: 'red'}}>{passwordError}</p> : ' '
+                }
             </div>
             <div>
                 <label>Confirm Password: </label>
                 <input type="text" onChange={(e) => setConfPassword(e.target.value)} />
+                {
+                    confpasswordError ? <p style={{color: 'red'}}>{confpasswordError}</p> : ' '
+                }
             </div>
             <input type="submit" value="Create User" />
         </form>
